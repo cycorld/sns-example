2ac83cec4eb0b06b63e64d9b9ae5e92396e7718b
cycorld <cycorld.com@gmail.com>
2017-09-12 22:12:40 +0900

    5-3-13. destroy, edit 액션 구현

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_ownership, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at desc')
@@ -13,5 +14,21 @@ class PostsController < ApplicationController
     else
       redirect_to new_post_path
     end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def check_ownership
+    @post = Post.find_by(id: params[:id])
+    redirect_to root_path if @post.user.id != current_user.id
   end
 end
