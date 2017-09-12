6533a4a2c3701ef1e578369e8411dc3aaadcbb8e
cycorld <cycorld.com@gmail.com>
2017-09-12 21:27:20 +0900

    5-3-9. posts#create 액션 작성

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    post = Post.new(user_id: current_user.id, content: params[:content])
+    if post.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path
+    end
+  end
 end
