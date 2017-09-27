f2b0406c6e828aa274e835d345e8443c90bbf2a7
cycorld <cycorld.com@gmail.com>
2017-09-27 16:07:38 +0900

    5-5-6. User 모델에 following 관계 설정

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :follower_relations, foreign_key: "followed_id", class_name: "Follow"
   has_many :followers, through: :follower_relations, source: :follower
+  has_many :following_relations, foreign_key: "follower_id", class_name: "Follow"
+  has_many :followings, through: :following_relations, source: :followed
 end
